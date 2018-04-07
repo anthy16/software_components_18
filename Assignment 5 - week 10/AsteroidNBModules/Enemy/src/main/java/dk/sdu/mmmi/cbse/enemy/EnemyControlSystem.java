ffabc3bedb526e9976575e1f859baea3f506e664
ANDERS\AKT <anthy16@student.sdu.dk>
2018-04-07 16:08:58 +0200

    Adding modules to netbeans lab 2

---
+++ Assignment 5 - week 10/AsteroidNBModules/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyControlSystem.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.enemy;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.Random;
+
+/**
+ *
+ * @author AKT
+ */
+public class EnemyControlSystem implements IEntityProcessingService, IGamePluginService {
+    
+    private Entity enemy;
+    
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            PositionPart positionPart = enemy.getPart(PositionPart.class);
+            MovingPart movingPart = enemy.getPart(MovingPart.class);
+
+            movingPart.setLeft(this.getMovement());
+            movingPart.setRight(this.getMovement());
+            movingPart.setUp(this.getMovement());
+            
+            
+            movingPart.process(gameData, enemy);
+            positionPart.process(gameData, enemy);
+
+            updateShape(enemy);
+        }
+    }
+    
+    private boolean getMovement() {
+        Random rdm = new Random();
+        
+        return rdm.nextBoolean();
+    }
+
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * 8);
+        shapey[0] = (float) (y + Math.sin(radians) * 8);
+
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
+
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
+
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+    
+    @Override
+    public void start(GameData gameData, World world) {
+        
+        // Add entities to the world
+        enemy = createPlayerShip(gameData);
+        world.addEntity(enemy);
+    }
+
+    private Entity createPlayerShip(GameData gameData) {
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 2;
+        float y = gameData.getDisplayHeight() / 2;
+        float radians = 3.1415f / 2;
+        
+        Entity playerShip = new Enemy();
+        playerShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        playerShip.add(new PositionPart(x, y, radians));
+        
+        return playerShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(enemy);
+    }
+    
+}
